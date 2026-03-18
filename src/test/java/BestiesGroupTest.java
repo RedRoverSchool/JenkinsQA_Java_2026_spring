b5866e2a066b178b48475206a06629c64fec6471
Юлия Драгун <y.dragun@automacon.ru>
2026-03-18 21:39:47 +0300

    besties new test

--- src/test/java/BestiesGroupTest.java
+++ src/test/java/BestiesGroupTest.java
@@ -2,8 +2,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.testng.AssertJUnit.assertEquals;
 
@@ -32,4 +35,34 @@ public class BestiesGroupTest {
 
         driver.quit();
     }
+
+    @Test
+    public void testWBbyOftenSearched(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.wildberries.by/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        driver.findElement(By.xpath("//div/ul/li[@data-menu-id=\"brands\"]")).click();
+        driver.findElement(By.id("searchInput")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(600));
+
+        WebElement input = driver.findElement(By.id("mobileSearchInput"));
+        Assert.assertEquals(input.getText(), "");
+
+        WebElement title = driver.findElement(By.className("autocomplete__title"));
+        Assert.assertEquals(title.getText(), "Часто ищут");
+
+        Map<Integer, String> bestRecomeds = new HashMap<>();
+        bestRecomeds.put(1, "кроссовки женские");
+        bestRecomeds.put(2, "джинсы женские");
+        bestRecomeds.put(3, "куртка демисезонная женская");
+        for (int key : bestRecomeds.keySet()) {
+            WebElement firstRecomend = driver.findElement(By.xpath("//div[@class=\"autocomplete\"]//li[" + key + "]//span[2]"));
+            Assert.assertEquals(firstRecomend.getText(), bestRecomeds.get(key));
+        }
+
+        driver.quit();
+    }
+
+
 }
