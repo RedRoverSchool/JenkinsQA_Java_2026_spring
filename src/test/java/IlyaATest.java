fddfc56f9015cb5d0ab9864249090c4c4887da9f
user <i.aliakseyeu@yandex.com>
2026-03-18 21:17:41 +0500

    ia-first-commit

--- src/test/java/IlyaATest.java
+++ src/test/java/IlyaATest.java
@@ -10,7 +10,7 @@ import java.time.Duration;
 public class IlyaATest {
 
     @Test
-    public void IlyaATest() {
+    public void firstSeleniumTest() {
 
         WebDriver driver = new FirefoxDriver();
 
