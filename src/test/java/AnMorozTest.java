6e04b761503b66efbfba3f1f1f38d4652d9c476b
Anna Morozova <morozanna503@gmail.com>
2026-03-24 13:30:23 +0700

    add new test

--- src/test/java/AnMorozTest.java
+++ src/test/java/AnMorozTest.java
@@ -27,4 +27,30 @@ public class AnMorozTest {
 
         driver.quit();
     }
+
+    @Test
+    public void testSecond() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://wooordhunt.ru/dic/content/en_ru");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement findLine = driver.findElement(By.id("hunted_word"));
+
+        findLine.sendKeys("cat");
+
+        WebElement buttonFind = driver.findElement(By.id("hunted_word_submit"));
+
+        buttonFind.click();
+
+        WebElement word = driver.findElement(By.xpath("//div[@id = 'wd_title']/h1"));
+
+        word.getText();
+
+        Assert.assertEquals(word.getText(), "Cat");
+
+        driver.quit();
+    }
+
 }
