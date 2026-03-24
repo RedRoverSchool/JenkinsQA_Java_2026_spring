a24c2db789ce386a1a28da9d97991988ad8b4cf2
Anna Morozova <morozanna503@gmail.com>
2026-03-24 13:38:05 +0700

    delete all empty Lines

--- src/test/java/AnMorozTest.java
+++ src/test/java/AnMorozTest.java
@@ -14,17 +14,12 @@ public class AnMorozTest {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
         WebElement submitButton = driver.findElement(By.cssSelector("button"));
-
         submitButton.click();
-
         WebElement message = driver.findElement(By.id("message"));
 
         Assert.assertEquals(message.getText(), "Received!");
-
         driver.quit();
     }
 
@@ -33,24 +28,15 @@ public class AnMorozTest {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://wooordhunt.ru/dic/content/en_ru");
-
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
         WebElement findLine = driver.findElement(By.id("hunted_word"));
-
         findLine.sendKeys("cat");
-
         WebElement buttonFind = driver.findElement(By.id("hunted_word_submit"));
-
         buttonFind.click();
-
         WebElement word = driver.findElement(By.xpath("//div[@id = 'wd_title']/h1"));
-
         word.getText();
 
         Assert.assertEquals(word.getText(), "Cat");
-
         driver.quit();
     }
-
 }
