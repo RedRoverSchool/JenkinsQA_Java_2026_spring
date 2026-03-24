bdffd79abacce46d7a8eb34a195831261bd861ab
Lidiya Shakirova <your_email@example.com>
2026-03-24 19:39:22 +0300

    add first test

---
+++ src/test/java/LidiyaTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LidiyaTest {
+    @Test
+    public void FurnitureStoreTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://bogatir.online/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
+
+        WebElement authButton = driver.findElement(By.xpath("//*[@class='w-inline-block login-header']"));
+        authButton.click();
+
+        WebElement inputEamil = driver.findElement(By.xpath("//*[@id=\"login3\"]"));
+        WebElement inputPassword = driver.findElement(By.xpath("//*[@id=\"password3\"]"));
+        inputEamil.sendKeys("cgvngv@ema");
+        inputPassword.sendKeys("46564");
+
+        WebElement inputButton = driver.findElement(By.xpath("//form[@id=\"login_form\"] //*[@type=\"submit\"]"));
+        inputButton.click();
+
+        WebElement errorMessage = driver.findElement(By.xpath("//*[@class=\"text-not-found\"]//*[@color=\"red\"]"));
+        Assert.assertEquals(errorMessage.getText(), "Неверный логин или пароль");
+
+        driver.quit();
+
+    }
+
+}
