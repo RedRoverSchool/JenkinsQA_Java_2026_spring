bbfe3bec0a9ad6ef4494f48abdeaa96bfced10d1
NataliaMiro <natalymir82@gmail.com>
2026-03-27 12:05:10 +0300

    Добавила 2 теста в класс

--- src/test/java/GroupFutureAqaTest.java
+++ src/test/java/GroupFutureAqaTest.java
@@ -1,9 +1,8 @@
 import jdk.jfr.Description;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -86,10 +85,76 @@ public class GroupFutureAqaTest {
         } finally {
             driver.quit();
         }
-
-
     }
 
+    @Test
+    @Description("Проверяет, что после поиска адреса \"Проспект Победы, 97\" в 2ГИС\n" +
+            " * открывается карточка организации с корректным URL.")
+    public void testRoutePlaningNaMironova() {
+
+        WebDriver driver = new ChromeDriver();
 
 
+        try {
+            driver.get("https://2gis.ru/penza");
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+            WebElement fromInput = wait.until(
+                    ExpectedConditions.visibilityOfElementLocated(
+                            By.xpath("//input[@placeholder='Поиск в 2ГИС']")
+                    )
+            );
+
+            fromInput.sendKeys("Проспект Победы, 97");
+            fromInput.sendKeys(Keys.ENTER);
+
+            wait.until(ExpectedConditions.urlContains("/geo/70030076158553552"));
+
+            String actualUrl = driver.getCurrentUrl();
+
+            String baseUrl = actualUrl.split("\\?")[0];
+            String expectedBase = "https://2gis.ru/penza/search/%D0%9F%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BA%D1%82%20%D0%9F%D0%BE%D0%B1%D0%B5%D0%B4%D1%8B%2C%2097/geo/70030076158553552/44.936159%2C53.229714";
+
+            Assert.assertEquals(expectedBase, baseUrl,
+                    "Базовая часть URL не совпадает");
+
+        } finally {
+            driver.quit();
+        }
+    }
+
+    @Test
+    @Description("Тест Передвижение слайдера")
+    public void testMoveSliderNaMironova() {
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.get("https://practice-automation.com/");
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+
+            WebElement slidersButton = wait.until(
+                    ExpectedConditions.elementToBeClickable(
+                            By.xpath("//a[.='Sliders']")
+                    )
+            );
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", slidersButton);
+
+            wait.until(ExpectedConditions.urlContains("slider"));
+            WebElement sliderMe =  wait.until(ExpectedConditions.visibilityOfElementLocated(
+                            By.id("slideMe"))
+            );
+
+            Actions actions = new Actions(driver);
+            actions.moveToElement(sliderMe, 250, 0).click().build().perform();
+            int width = sliderMe.getSize().getWidth();
+            actions.moveToElement(sliderMe, (width/2) - 100, 0 ).click().build().perform();
+
+
+        } finally {
+            driver.quit();
+        }
+    }
 }
