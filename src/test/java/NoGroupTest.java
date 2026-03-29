9d4bcfcba2b8993f041716649aded2bf7a3aadc9
Natalia Barakova <Garradegaruda@gmail.com>
2026-03-29 10:58:04 +0300

    Delete class and move tests to NoGroupTest

--- src/test/java/NoGroupTest.java
+++ src/test/java/NoGroupTest.java
@@ -372,9 +372,7 @@ public class NoGroupTest {
 
         WebDriver driver = new ChromeDriver();
         try{
-
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
 
         driver.findElement(By.cssSelector("button")).click();
@@ -393,7 +391,6 @@ public class NoGroupTest {
         WebDriver driver = new ChromeDriver();
         try{
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
 
         WebElement mainTitle = driver.findElement(By.xpath("//h1[@class='display-6']"));
