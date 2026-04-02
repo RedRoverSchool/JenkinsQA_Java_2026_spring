817fd7562a164a3a2ca64edd23cf50919e3ca636
Yakov Tereshenkov <tereshenkov@gmail.com>
2026-04-03 00:50:33 +0300

    add waituntil to fix StaleElementReference

--- src/test/java/school/redrover/ChangeThemeTest.java
+++ src/test/java/school/redrover/ChangeThemeTest.java
@@ -2,16 +2,13 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
 import java.time.Duration;
-
-import static java.sql.DriverManager.getDriver;
 
 public class ChangeThemeTest extends BaseTest {
 
@@ -20,7 +17,12 @@ public class ChangeThemeTest extends BaseTest {
 
 
         getDriver().findElement(By.xpath("//*[@id='root-action-UserAction']")).click();
-        getDriver().findElement(By.xpath("//a[@href='/user/admin/appearance']")).click();
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//a[@href='/user/admin/appearance']"))).click();
+
+//        getDriver().findElement(By.xpath("//a[@href='/user/admin/appearance']")).click();
         getDriver().findElement(By.xpath("//label[@for='radio-block-1']")).click();
         getDriver().findElement
                 (By.xpath("//button[@class='jenkins-button apply-button']")).click();
