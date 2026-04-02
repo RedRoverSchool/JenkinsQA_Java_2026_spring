f2e2157984eb064aaf093d00859c421dca31f73f
NataliaMiro <natalymir82@gmail.com>
2026-04-02 11:09:20 +0300

    add class PlusButtonShowsNewItemTest

--- src/test/java/school/redrover/PlusButtonShowsNewItemTest.java
+++ src/test/java/school/redrover/PlusButtonShowsNewItemTest.java
@@ -6,9 +6,9 @@ import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
 
-public class CreateNewItemTest extends BaseTest {
+public class PlusButtonShowsNewItemTest extends BaseTest {
     @Test
-    public void testCreateNewItem(){
+    public void testClickingPlusButtonShowsNewItemTitle(){
             getDriver().findElement(By.xpath(
                     "//span[@class='task-link-text' and text()='New Item']/ancestor::span[@class='task-link-wrapper ']"))
                     .click();
