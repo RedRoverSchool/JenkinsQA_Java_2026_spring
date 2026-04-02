99f4073f239ecca161fef51b207c400f55196488
Lidiya Shakirova <your_email@example.com>
2026-04-02 23:57:51 +0300

    change test

--- src/test/java/school/redrover/ChangeThemeTest.java
+++ src/test/java/school/redrover/ChangeThemeTest.java
@@ -18,13 +18,18 @@ public class ChangeThemeTest extends BaseTest {
     @Test
     public void testChangeDarkTheme(){
 
+
         getDriver().findElement(By.xpath("//*[@id='root-action-UserAction']")).click();
         getDriver().findElement(By.xpath("//a[@href='/user/admin/appearance']")).click();
         getDriver().findElement(By.xpath("//label[@for='radio-block-1']")).click();
         getDriver().findElement
                 (By.xpath("//button[@class='jenkins-button jenkins-submit-button jenkins-button--primary ']")).click();
 
-        
+        new WebDriverWait(getDriver(), Duration.ofSeconds(2))
+                .until(driver -> ((JavascriptExecutor) driver)
+                                .executeScript("return document.documentElement.getAttribute('data-theme')")
+                                .equals("dark"));
+
 
         Assert.assertEquals("dark",
                 ((JavascriptExecutor) getDriver()).executeScript("return document.documentElement.getAttribute('data-theme')")
