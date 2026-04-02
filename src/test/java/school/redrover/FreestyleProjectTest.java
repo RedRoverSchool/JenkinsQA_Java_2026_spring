edb2bf981c7d360c67f57a85b09fb1049301918c
Anna Kuryleva <annak12111974@gmail.com>
2026-04-02 22:10:15 +0300

    fix testCreateFreestyleProject

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -1,14 +1,16 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
+import java.time.Duration;
+
 public class FreestyleProjectTest extends BaseTest {
 
-    @Ignore
     @Test
     public void testCreateFreestyleProject() {
         String testProjectName = "test";
@@ -17,7 +19,9 @@ public class FreestyleProjectTest extends BaseTest {
         getDriver().findElement(By.id("name")).sendKeys(testProjectName);
         getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
         getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.xpath("//a[@class='app-jenkins-logo']")).click();
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//a[@class='app-jenkins-logo']"))).click();
 
         Assert.assertEquals(getDriver().findElement(
                 By.xpath("//*[@class='jenkins-table__link model-link inside']")).getText(),
