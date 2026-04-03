742c4fea9e1ceab3600e76e6af1c54af676cec79
Iurii <yuri.ivanoff@gmail.com>
2026-04-03 16:28:50 +0300

    Create class AccountPageTest. Add method testGetAccountPagePath1.

--- src/test/java/school/redrover/AccountPageTest.java
+++ src/test/java/school/redrover/AccountPageTest.java
@@ -1,22 +1,16 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
-
-import java.time.Duration;
 
 public class AccountPageTest extends BaseTest {
 
     @Test
     public void testGetAccountPagePath1() {
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
-
         getDriver().findElement(By.id("root-action-UserAction")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/user/user/account']"))).click();
+        getDriver().findElement(By.xpath("//a[starts-with(@href, '/user/') and contains(@href, '/account')]")).click();
 
         Assert.assertEquals(
                 getDriver().findElement(By.xpath("//*[@id='main-panel']//h1")).getText().trim(),
