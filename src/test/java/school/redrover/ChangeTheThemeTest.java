bc43b530f9b6276a2eaa794c6588cf0d4a856d5c
Alesia <delta_29@mail.ru>
2026-04-03 22:25:58 +0300

    new test Сhecking The Theme

--- src/test/java/school/redrover/ChangeTheThemeTest.java
+++ src/test/java/school/redrover/ChangeTheThemeTest.java
@@ -2,8 +2,6 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
@@ -15,16 +13,11 @@ public class ChangeTheThemeTest extends BaseTest {
     @Test
     public void testChangeTheTheme () {
         getDriver().findElement(By.id("root-action-UserAction")).click();
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(15));
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//*[@id='tasks']/div[5]/span/a")));
-        element.click();
-        getDriver().findElement(By.xpath("//*[@id='tasks']/div[5]/span/a")).click();
+        getDriver().findElement(By.xpath("/html/body/div[2]/div[1]/div[2]/div[5]/span/a")).click();
         getDriver().findElement(By.xpath("//label[@for='radio-block-1']")).click();
 
         WebElement html = getDriver().findElement(By.tagName("html"));
-        String theme = html.getAttribute("data-theme");
 
-        Assert.assertEquals(theme, "dark");
+        Assert.assertEquals(html.getAttribute("data-theme"), "dark");
     }
 }
