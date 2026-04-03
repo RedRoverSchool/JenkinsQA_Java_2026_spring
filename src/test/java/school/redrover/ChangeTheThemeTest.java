23998b71b2bc1ac25790996d071fc07f5c9643d2
Alesia <delta_29@mail.ru>
2026-04-03 19:57:49 +0300

    new test Сhecking The Theme

---
+++ src/test/java/school/redrover/ChangeTheThemeTest.java
@@ -0,0 +1,23 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class ChangeTheThemeTest extends BaseTest {
+
+    @Test
+    public void testChangeTheTheme () {
+
+        getDriver().findElement(By.id("root-action-UserAction")).click();
+        getDriver().findElement(By.cssSelector("#tasks > div:nth-child(5) > span > a")).click();
+        getDriver().findElement(By.xpath("//label[@for='radio-block-1']")).click();
+
+        WebElement html = getDriver().findElement(By.tagName("html"));
+        String theme = html.getAttribute("data-theme");
+
+        Assert.assertEquals(theme, "dark");
+    }
+}
