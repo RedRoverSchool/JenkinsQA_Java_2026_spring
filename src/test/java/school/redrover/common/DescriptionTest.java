c1e30c3dbae0f934a35824b3bc82088c2ade4d74
Fedor.Streha <fstrecha1993@gmail.com>
2026-04-03 20:49:46 +0300

    new test

---
+++ src/test/java/school/redrover/common/DescriptionTest.java
@@ -0,0 +1,18 @@
+package school.redrover.common;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DescriptionTest extends BaseTest{
+
+    @Test
+
+    public void testDescription () {
+        getDriver().findElement(By.id("description-link")).click();
+        getDriver().findElement(By.xpath("//*[@id='description-edit-form']/form/div[1]/div[1]/textarea")).sendKeys("hi");
+        getDriver().findElement(By.xpath("//*[@id='description-edit-form']/form/div[2]/button[1]")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//*[@id='description-content']")).getText(),"hi");
+    }
+}
