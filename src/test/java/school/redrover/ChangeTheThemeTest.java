18beb888f6f76674b5b1bc7c824d3ef8f38fd620
Fedor.Streha <fstrecha1993@gmail.com>
2026-04-03 22:58:34 +0300

    new test2

--- src/test/java/school/redrover/ChangeTheThemeTest.java
+++ src/test/java/school/redrover/ChangeTheThemeTest.java
@@ -2,17 +2,27 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+
+import java.time.Duration;
 
 public class ChangeTheThemeTest extends BaseTest {
 
     @Test
     public void testChangeTheTheme () {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
 
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("root-action-UserAction")));
         getDriver().findElement(By.id("root-action-UserAction")).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@href,'appearance')]")));
         getDriver().findElement(By.xpath("//a[contains(@href,'appearance')]")).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[@for='radio-block-1']")));
         getDriver().findElement(By.xpath("//label[@for='radio-block-1']")).click();
 
         WebElement html = getDriver().findElement(By.tagName("html"));
