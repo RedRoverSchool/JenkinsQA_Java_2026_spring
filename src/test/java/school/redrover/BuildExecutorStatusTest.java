ad2284329b245a0b23eae6198d373d92d9864c52
Eni11-eni <enigul00@mail.ru>
2026-04-03 22:38:37 +0500

    add new test

---
+++ src/test/java/school/redrover/BuildExecutorStatusTest.java
@@ -0,0 +1,21 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class BuildExecutorStatusTest extends BaseTest {
+
+    @Test
+    public void testBuildExecutorStatus() {
+
+        WebElement buildExecutorStatus = getDriver().findElement(By.xpath("//a[@href='/computer/']"));
+        buildExecutorStatus.click();
+
+        WebElement title = getDriver().findElement(By.xpath("//h1"));
+
+        Assert.assertEquals(title.getText(), "Nodes");
+    }
+}
