8637c5d64969d504e61e4d2c1ba2fa747bf70e94
Fedor.Streha <fstrecha1993@gmail.com>
2026-04-03 23:07:10 +0300

    new test2

--- src/test/java/school/redrover/ChangeTheThemeTest.java
+++ src/test/java/school/redrover/ChangeTheThemeTest.java
@@ -16,14 +16,9 @@ public class ChangeTheThemeTest extends BaseTest {
     public void testChangeTheTheme () {
         WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("root-action-UserAction")));
-        getDriver().findElement(By.id("root-action-UserAction")).click();
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@href,'appearance')]")));
-        getDriver().findElement(By.xpath("//a[contains(@href,'appearance')]")).click();
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[@for='radio-block-1']")));
-        getDriver().findElement(By.xpath("//label[@for='radio-block-1']")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("root-action-UserAction"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@href,'appearance')]"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[@for='radio-block-1']"))).click();
 
         WebElement html = getDriver().findElement(By.tagName("html"));
 
