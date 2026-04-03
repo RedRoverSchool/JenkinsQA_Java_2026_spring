799c2b1445e8f68939d91b37cfa1832812c04e4c
Alesia <delta_29@mail.ru>
2026-04-03 22:16:27 +0300

    new test Сhecking The Theme

--- src/test/java/school/redrover/ChangeTheThemeTest.java
+++ src/test/java/school/redrover/ChangeTheThemeTest.java
@@ -14,18 +14,16 @@ public class ChangeTheThemeTest extends BaseTest {
 
     @Test
     public void testChangeTheTheme () {
-
         getDriver().findElement(By.id("root-action-UserAction")).click();
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(15));
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//*[@id='tasks']/div[5]/span/a")));
+        element.click();
         getDriver().findElement(By.xpath("//*[@id='tasks']/div[5]/span/a")).click();
         getDriver().findElement(By.xpath("//label[@for='radio-block-1']")).click();
 
         WebElement html = getDriver().findElement(By.tagName("html"));
         String theme = html.getAttribute("data-theme");
-
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(15));
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//*[@id='tasks']/div[5]/span/a")));
-        element.click();
 
         Assert.assertEquals(theme, "dark");
     }
