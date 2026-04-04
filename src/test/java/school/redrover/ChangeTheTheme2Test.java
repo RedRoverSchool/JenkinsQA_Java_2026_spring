95b5e49cf923b964aec7629750b791a47af3b985
Alesia <delta_29@mail.ru>
2026-04-04 17:17:03 +0300

    new test Сhecking The Theme

--- src/test/java/school/redrover/ChangeTheTheme2Test.java
+++ src/test/java/school/redrover/ChangeTheTheme2Test.java
@@ -2,15 +2,12 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-import java.time.Duration;
-
 public class ChangeTheTheme2Test extends BaseTest {
+
     @Test
     public void testChangeTheTheme () {
         getDriver().findElement(By.id("root-action-UserAction")).click();
