6d023349b4df098648601495e08a4281155ee682
ivan-brv <iberov016@gmail.com>
2026-04-05 12:55:10 +0300

    add class CheckAvailableItemsTest

--- src/test/java/school/redrover/CheckAvailableItemsTest.java
+++ src/test/java/school/redrover/CheckAvailableItemsTest.java
@@ -11,18 +11,21 @@ import java.util.List;
 public class CheckAvailableItemsTest extends BaseTest {
 
     @Test
-    public void checkAvailableItems(){
+    public void testCheckAvailableItems(){
 
-        final List<String> expectedItems = List.of("Freestyle project", "Pipeline", "Multi-configuration project",
-                "Folder", "Multibranch Pipeline", "Organization Folder");
+        List<String> expectedItems = new ArrayList<>(List.of("Freestyle project", "Pipeline", "Multi-configuration project",
+                "Folder", "Multibranch Pipeline", "Organization Folder"));
         List<String> actualItems = new ArrayList<>();
 
         getDriver().findElement(By.xpath("//span[text()='Create a job']")).click();
-        List<WebElement> actualWebItems = getDriver().findElements(By.xpath("//li[@tabindex='0']//span[@class='label']"));
+        List<WebElement> actualWebItems = getDriver().findElements(By.xpath("//div[@id='items']//span[@class='label']"));
 
         for (WebElement i : actualWebItems) {
             actualItems.add(i.getText());
         }
+
+        expectedItems.sort(String::compareTo);
+        actualItems.sort(String::compareTo);
 
         Assert.assertEquals(actualItems, expectedItems);
     }
