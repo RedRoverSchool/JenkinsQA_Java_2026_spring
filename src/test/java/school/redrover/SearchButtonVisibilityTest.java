064fd10dae93e90119cb8f883ba54cd5927053bd
user <i.aliakseyeu@yandex.com>
2026-04-06 21:41:09 +0500

    Search-button-test-add

---
+++ src/test/java/school/redrover/SearchButtonVisibilityTest.java
@@ -0,0 +1,19 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class SearchButtonVisibilityTest extends BaseTest {
+
+    @Test
+    public void testSearchButton() {
+
+        getDriver().findElement(By.xpath("//button[@id='root-action-SearchAction']")).click();
+
+        Assert.assertTrue(
+            getDriver().findElement(By.xpath("//input")).isDisplayed());
+
+    }
+}
