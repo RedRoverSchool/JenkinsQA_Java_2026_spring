91d8b865dcb1758d7a1db97f3d51de5fcc20e6cc
Olga <khairutdinova.oi@gmail.com>
2026-04-07 12:05:45 +0300

    тест для ссылки REST API в Footer (открывается в том же окне) TC_15.002.04 | Footer > REST API>Checking Same Tab Opening

--- src/test/java/school/redrover/RestApiSameTabTest.java
+++ src/test/java/school/redrover/RestApiSameTabTest.java
@@ -29,32 +29,10 @@ public class RestApiSameTabTest extends BaseTest {
 
         restApiLink.click();
 
-
         Assert.assertEquals(getDriver().getWindowHandle(), originalWindow,
                 "Фокус переключился на другое окно");
 
     }
-    @Test
-    public void testRestApiLinkIsHiddenOnApiPage(){
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
-        JavascriptExecutor js = (JavascriptExecutor) getDriver();
-
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-
-       wait.until(
-                ExpectedConditions.elementToBeClickable(By.xpath("//footer//a[contains(text(),'REST API')]"))
-        ).click();
-
-       js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-
-
-        boolean isRestApiLinkPresentInFooter = !getDriver().findElements(
-                By.xpath("//footer//a[contains(text(),'REST API')]")
-        ).isEmpty();
-
-        Assert.assertFalse(isRestApiLinkPresentInFooter,
-                "Ссылка 'REST API' не должна отображаться в футере на странице REST API (сама на себя)");
-    }
 }
 
