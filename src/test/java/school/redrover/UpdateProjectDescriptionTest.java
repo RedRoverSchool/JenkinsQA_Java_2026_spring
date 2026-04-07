3df759f945d384f1c1de7f5474fb41b70c04189e
ivan-brv <iberov016@gmail.com>
2026-04-07 20:44:57 +0300

    add class UpdateProjectDescriptionTest

--- src/test/java/school/redrover/UpdateProjectDescriptionTest.java
+++ src/test/java/school/redrover/UpdateProjectDescriptionTest.java
@@ -12,7 +12,7 @@ public class UpdateProjectDescriptionTest extends BaseTest {
     @Test
     public void testUpdateProjectDescription() {
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(3));
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
 
         String expectedDescription = "The main goal of the project is to return Manchester United to its former glory";
 
@@ -20,7 +20,7 @@ public class UpdateProjectDescriptionTest extends BaseTest {
         getDriver().findElement(By.xpath("//span[text()='Freestyle project']")).click();
         getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("Make Man Utd great again");
         getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
-        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys("First test description");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//textarea[@name='description']"))).sendKeys("First test description");
         getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
         getDriver().findElement(By.xpath("//a[@id='description-link']")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//textarea[@name='description']"))).clear();
