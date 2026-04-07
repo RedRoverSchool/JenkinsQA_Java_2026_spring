46c7d6b9d355d5a553494755e8cc0248b6aff8c8
Fedor.Streha <fstrecha1993@gmail.com>
2026-04-07 22:08:55 +0300

    add test Multi-Configuration Project #309

--- src/test/java/school/redrover/MultiConfigurationProjectTest.java
+++ src/test/java/school/redrover/MultiConfigurationProjectTest.java
@@ -15,12 +15,15 @@ public class MultiConfigurationProjectTest extends BaseTest {
 
     public void testMultiConfigurationProject () {
 
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@it='hudson.model.Hudson@297db9bc']")));
+
         getDriver().findElement(By.xpath("//a[@it='hudson.model.Hudson@297db9bc']")).click();
         getDriver().findElement(By.id("name")).sendKeys("test");
         getDriver().findElement(By.xpath("//li[@class='hudson_matrix_MatrixProject']")).click();
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(3));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ok-button")));
+        WebDriverWait wait1 = new WebDriverWait(getDriver(), Duration.ofSeconds(3));
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("ok-button")));
 
         Assert.assertTrue(getDriver().findElement(By.id("ok-button")).isEnabled());
     }
