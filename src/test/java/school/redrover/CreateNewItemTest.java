7fb1236f460fa008d239584f38af1df09a278eb4
ar4i-helloWorld <vitalymel9@gmail.com>
2026-04-07 20:06:44 +0300

    Add TC_01.001.07 test

--- src/test/java/school/redrover/CreateNewItemTest.java
+++ src/test/java/school/redrover/CreateNewItemTest.java
@@ -1,36 +1,19 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
-import java.util.ArrayList;
-import java.util.List;
 
 public class CreateNewItemTest extends BaseTest {
-
     @Test
-    public void testCheckItemTypesForSelect() {
-        final List<String> expectedItemTypeList = List.of(
-                "Pipeline",
-                "Freestyle project",
-                "Multi-configuration project",
-                "Folder",
-                "Multibranch Pipeline",
-                "Organization Folder"
-        );
+    public void testCreateNewItem() throws InterruptedException {
+        String expectedText = "New Item - Jenkins";
+        String actualText;
 
-        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.xpath("//div[@class='task ']")).click();
+        actualText = getDriver().getTitle();
 
-        List<WebElement> itemTypeList = getDriver().findElements(By.xpath("//span[@class='label']"));
-
-        List<String> actualItemTypeList = new ArrayList<>();
-        Assert.assertNotEquals(itemTypeList.size(), 0);
-        for (WebElement element : itemTypeList) {
-            actualItemTypeList.add(element.getText());
-        }
-
-        Assert.assertEquals(expectedItemTypeList, actualItemTypeList);
+        Assert.assertEquals(actualText, expectedText);
     }
 }
