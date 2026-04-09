248b8b10960b3709cf16add618cadaa61a44f078
Olga <khairutdinova.oi@gmail.com>
2026-04-09 13:00:42 +0300

    Создала класс SingOut2Test.java для US_08.002 написала тест кейс. этот автотест проверяет что разлогинивание происходит моментально и еще удалила класс не привязанный к тест кейсу

---
+++ src/test/java/school/redrover/SingOut2Test.java
@@ -0,0 +1,53 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+import java.time.Duration;
+
+public class SingOut2Test extends BaseTest {
+
+    private boolean isAlertPresent(WebDriver driver) {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    @Test
+    public void testSingOutIsImmediate() {
+
+        // 1. Находим кнопку пользователя
+        WebElement userButton = getDriver().findElement(By.id("root-action-UserAction"));
+
+        // 2. Наводим мышь для открытия выпадающего меню
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(userButton).perform();
+
+        // 3. Ждём появления меню и кликаем "Sign out"
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        WebElement dropdownMenu = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='jenkins-dropdown']"))
+        );
+        dropdownMenu.findElement(By.xpath(".//a[@href='/logout']")).click();
+
+        // 4. Проверяем, что alert не появился (используем вспомогательный метод)
+        Assert.assertFalse(isAlertPresent(getDriver()),
+                "Не должно быть alert-окна подтверждения выхода. Выход должен быть мгновенным.");
+
+        // 5. Ждём перенаправления на страницу логина
+        wait.until(ExpectedConditions.urlContains("login"));
+
+        System.out.println("Тест пройден: выход выполнен мгновенно, без подтверждений.");
+    }
+}
