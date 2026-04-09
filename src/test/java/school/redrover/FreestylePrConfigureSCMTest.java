7ddeb9bfeb734075e6fc815e136d54312cba674f
Alesia <delta_29@mail.ru>
2026-04-09 19:53:39 +0300

    add new testRepositoryURL,testCredentials, testBranchesToBuild #385

--- src/test/java/school/redrover/FreestylePrConfigureSCMTest.java
+++ src/test/java/school/redrover/FreestylePrConfigureSCMTest.java
@@ -4,26 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-import java.time.Duration;
-
 public class FreestylePrConfigureSCMTest extends BaseTest {
-
-    @Test
-    public void testRepositoryURL() {
+    private void createFreestyleProjectAndSelectGit(String projectName) {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("Freestyle");
+        getDriver().findElement(By.id("name")).sendKeys(projectName);
         getDriver().findElement(By.cssSelector("li.hudson_model_FreeStyleProject")).click();
         getDriver().findElement(By.id("ok-button")).click();
+
         WebElement gitOption = getDriver().findElement(By.xpath("//label[text()='Git']"));
         ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", gitOption);
+    }
+    @Test
+    public void testRepositoryURL() {
+        createFreestyleProjectAndSelectGit("Freestyle");
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("_.url"))).
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.name("_.url"))).
                 sendKeys("https://github.com");
 
         Assert.assertEquals(getDriver().findElement(By.name("_.url")).
@@ -31,30 +30,18 @@ public class FreestylePrConfigureSCMTest extends BaseTest {
     }
     @Test
     public void testCredentials() {
-        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("Freestyle");
-        getDriver().findElement(By.cssSelector("li.hudson_model_FreeStyleProject")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        WebElement gitOption = getDriver().findElement(By.xpath("//label[text()='Git']"));
-        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", gitOption);
+        createFreestyleProjectAndSelectGit("Freestyle");
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
-        WebElement dropDownList = wait.until(ExpectedConditions.visibilityOfElementLocated(
+        WebElement dropDownList = getWait5().until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//select[@name='_.credentialsId']")));
 
         Assert.assertTrue(dropDownList.isDisplayed(), "The Credentials drop-down list is not displayed");
     }
     @Test
     public void testBranchesToBuild() {
-        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("Freestyle");
-        getDriver().findElement(By.cssSelector("li.hudson_model_FreeStyleProject")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        WebElement gitOption = getDriver().findElement(By.xpath("//label[text()='Git']"));
-        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", gitOption);
+        createFreestyleProjectAndSelectGit("Freestyle");
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
-        WebElement branchInput = wait.until(ExpectedConditions.visibilityOfElementLocated
+        WebElement branchInput = getWait5().until(ExpectedConditions.visibilityOfElementLocated
                 (By.xpath("//div[contains(text(), 'Branch Specifier')]/following::input[1]")));
         branchInput.clear();
         branchInput.sendKeys("*/main");
