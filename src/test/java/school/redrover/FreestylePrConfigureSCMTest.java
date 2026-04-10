3ee951f8374d699593c70a3e91d306b4598926df
Alesia <delta_29@mail.ru>
2026-04-10 16:17:40 +0300

    add newtestSCMAuthenticationFails #428

--- src/test/java/school/redrover/FreestylePrConfigureSCMTest.java
+++ src/test/java/school/redrover/FreestylePrConfigureSCMTest.java
@@ -4,9 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+
+import java.time.Duration;
 
 public class FreestylePrConfigureSCMTest extends BaseTest {
     private void createFreestyleProjectAndSelectGit() {
@@ -18,12 +21,14 @@ public class FreestylePrConfigureSCMTest extends BaseTest {
         WebElement gitOption = getDriver().findElement(By.xpath("//label[text()='Git']"));
         ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", gitOption);
     }
+    private void enterRepositoryURL(){
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.name("_.url"))).
+                sendKeys("https://github.com");
+    }
     @Test
     public void testRepositoryURL() {
         createFreestyleProjectAndSelectGit();
-
-        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.name("_.url"))).
-                sendKeys("https://github.com");
+        enterRepositoryURL();
 
         Assert.assertEquals(getDriver().findElement(By.name("_.url")).
                 getAttribute("value"), "https://github.com", "The repository URL does not match!");
@@ -50,5 +55,23 @@ public class FreestylePrConfigureSCMTest extends BaseTest {
                         (By.xpath("//div[contains(text(), 'Branch Specifier')]/following::input[1]")).
                 getAttribute("value"), "*/main",
                 "The branch name does not match the expected one!");
+    }
+    @Test
+    public void testSCMAuthenticationFails(){
+        createFreestyleProjectAndSelectGit();
+        enterRepositoryURL();
+
+        getDriver().findElement(By.id("page-body")).click();
+
+        getWait10().until(ExpectedConditions.textToBePresentInElementLocated(
+                By.xpath("//input[@name='_.url']/following::div[@class='error'][1]"),
+                "Failed to connect to repository"));
+
+        String actualError = getDriver().findElement
+                (By.xpath("//input[@name='_.url']/following::div[@class='error'][1]")).getText();
+
+        Assert.assertTrue(actualError.contains("Failed to connect to repository"),
+                "Ожидаемый текст ошибки не найден" + actualError);
+
     }
 }
