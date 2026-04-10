2b9132e6c4e8100f52225e060ca1b621a7555600
Petr P <none@none.none>
2026-04-10 12:13:45 +0800

    create test login with invalid username

--- src/test/java/school/redrover/SingInTest.java
+++ src/test/java/school/redrover/SingInTest.java
@@ -11,6 +11,11 @@ import school.redrover.common.BaseTest;
 
 public class SingInTest extends BaseTest {
 
+    final String userLogin = "Berendey";
+    final String userPassword = "Beren123";
+    final String userFullName = "Berendey";
+    final String userEMail = "berendey@kingdom.pz";
+
     private void logout(){
 
         WebElement userButton = getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.id("root-action-UserAction")));
@@ -24,18 +29,14 @@ public class SingInTest extends BaseTest {
     @Test
     public void testLoginValidData () {
 
-        final String userLogin = "KozhemiakaN";
-        final String userPassword = "Nik123";
-
         TestUtility.createUser(userLogin,
-                "Nikita",
+                userFullName,
                 userPassword,
                 userPassword,
-                "kozhemiaka@nikita.da",
+                userEMail,
                 getDriver());
 
         logout();
-
 
         getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.className("app-sign-in-register__content-inner")));
 
@@ -51,23 +52,42 @@ public class SingInTest extends BaseTest {
     @Test
     public void testLoginInvalidPassword () {
 
-        final String userLogin = "Berendey";
-        final String userPassword = "Beren123";
-
         TestUtility.createUser(userLogin,
-                "Berendey",
+                userFullName,
                 userPassword,
                 userPassword,
-                "berendey@kingdom.pz",
+                userEMail,
                 getDriver());
 
         logout();
-
 
         getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.className("app-sign-in-register__content-inner")));
 
         getDriver().findElement(By.name("j_username")).sendKeys(userLogin);
         getDriver().findElement(By.name("j_password")).sendKeys("nik123");
+        getDriver().findElement(By.name("Submit")).click();
+
+        WebElement errorMessage = getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.className("app-sign-in-register__error")));
+
+        Assert.assertEquals(errorMessage.getText(), "Invalid username or password");
+    }
+
+    @Test
+    public void testLoginInvalidUsername () {
+
+        TestUtility.createUser(userLogin,
+                userFullName,
+                userPassword,
+                userPassword,
+                userEMail,
+                getDriver());
+
+        logout();
+
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.className("app-sign-in-register__content-inner")));
+
+        getDriver().findElement(By.name("j_username")).sendKeys("SpongeBob");
+        getDriver().findElement(By.name("j_password")).sendKeys(userPassword);
         getDriver().findElement(By.name("Submit")).click();
 
         WebElement errorMessage = getWait2().until(ExpectedConditions.visibilityOfElementLocated(By.className("app-sign-in-register__error")));
