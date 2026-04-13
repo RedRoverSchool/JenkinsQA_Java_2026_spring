110a4492cb9d6b91799fd3f60d3d9045ac95f1df
Alesia <delta_29@mail.ru>
2026-04-13 22:15:14 +0300

    add testAddCredentialsClick #519

--- src/test/java/school/redrover/AddCredentialsTest.java
+++ src/test/java/school/redrover/AddCredentialsTest.java
@@ -1,19 +1,40 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
 public class AddCredentialsTest extends BaseTest {
-    @Test
-    public void testAddCredentialsButtonActive(){
+
+    private static final String ADD_CREDENTIALS_BUTTON = "//button[contains(text(),'Add')]";
+    private static final String MODAL_TITLE_XPATH = "//dialog//*[contains(text(), 'Add Credentials')]";
+
+    private void credentialsOpen(){
         getDriver().findElement(By.id("root-action-ManageJenkinsAction")).click();
         getDriver().findElement(By.xpath("//a[@href='credentials']")).click();
+    }
+    private WebElement modalWindow(){
+       return getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath(MODAL_TITLE_XPATH)));
+    }
+    @Test
+    public void testAddCredentialsButtonActive(){
+        credentialsOpen();
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='main-panel']//h1")).getText(),
                 "Credentials");
-        Assert.assertTrue(getDriver().findElement(By.xpath("//button[contains(text(),'Add')]")).isEnabled(),
-                "'Add credentials' button is active!");
+        Assert.assertTrue(getDriver().findElement(By.xpath(ADD_CREDENTIALS_BUTTON)).isEnabled(),
+                "'Add credentials' button is not enabled!");
+    }
+    @Test
+    public void testAddCredentialsClick(){
+        credentialsOpen();
+
+        getDriver().findElement(By.xpath(ADD_CREDENTIALS_BUTTON)).click();
+        modalWindow();
+
+        Assert.assertTrue(modalWindow().isDisplayed(), "The 'Add credentials' modal window did not open!");
     }
 }
