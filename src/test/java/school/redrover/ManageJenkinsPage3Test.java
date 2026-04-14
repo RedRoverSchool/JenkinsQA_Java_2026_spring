2383be6b53fb4b8d3d82a1b660b9396fb6e9f9ca
NataliaMiro <natalymir82@gmail.com>
2026-04-14 19:09:52 +0300

    Исправлен тест Preview: добавлен dispatchEvent для ввода текста в textarea

--- src/test/java/school/redrover/ManageJenkinsPage3Test.java
+++ src/test/java/school/redrover/ManageJenkinsPage3Test.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -86,11 +87,9 @@ public class ManageJenkinsPage3Test extends BaseTest {
 
     @Test
     public void testPreviewButtonDisplaysFormattedSystemMessage() {
-        getWait10().until(ExpectedConditions.elementToBeClickable(
-                MANAGE_JENKINS_LINK)).click();
+        getWait10().until(ExpectedConditions.elementToBeClickable(MANAGE_JENKINS_LINK)).click();
 
-        getWait5().until(ExpectedConditions.elementToBeClickable(
-                CONFIGURE_SECURITY_LINK)).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(CONFIGURE_SECURITY_LINK)).click();
         getWait10().until(ExpectedConditions.urlContains("/configureSecurity"));
 
         Select markupFormatter = new Select(
@@ -102,28 +101,39 @@ public class ManageJenkinsPage3Test extends BaseTest {
         ((JavascriptExecutor) getDriver()).executeScript(
                 "window.scrollTo(0, document.body.scrollHeight);"
         );
-        getWait10().until(ExpectedConditions.elementToBeClickable(
-                By.name("Submit"))).click();
+        getWait10().until(ExpectedConditions.elementToBeClickable(By.name("Submit"))).click();
         getWait10().until(ExpectedConditions.urlContains("/manage/"));
 
-        getWait10().until(ExpectedConditions.elementToBeClickable(
-                CONFIGURE_SYSTEM_LINK)).click();
+        getWait10().until(ExpectedConditions.elementToBeClickable(CONFIGURE_SYSTEM_LINK)).click();
         getWait10().until(ExpectedConditions.urlContains("/configure"));
 
-        getWait10().until(ExpectedConditions.presenceOfElementLocated(
-                By.cssSelector("textarea[name='system_message']")));
+        // Работаем с textarea
+        WebElement textarea = getWait10().until(
+                ExpectedConditions.presenceOfElementLocated(By.cssSelector("textarea[name='system_message']"))
+        );
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(30));
 
-        WebElement codeMirror = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".CodeMirror"))
+        ((JavascriptExecutor) getDriver()).executeScript(
+                "arguments[0].dispatchEvent(new Event('input', { bubbles: true }));",
+                textarea
+        );
+
+        WebElement codeMirror = getWait10().until(
+                ExpectedConditions.presenceOfElementLocated(By.cssSelector(".CodeMirror"))
         );
         codeMirror.click();
 
-        String testMessage = "<b>Bold</b> <i>Italic</i>";
         new Actions(getDriver())
-                .sendKeys(testMessage)
+                .keyDown(Keys.CONTROL)
+                .sendKeys("a")
+                .keyUp(Keys.CONTROL)
+                .sendKeys(Keys.DELETE)
                 .perform();
+
+        String testMessage = "<b>Bold</b> <i>Italic</i>";
+        for (char c : testMessage.toCharArray()) {
+            new Actions(getDriver()).sendKeys(String.valueOf(c)).perform();
+        }
 
         WebElement previewLink = getWait5().until(
                 ExpectedConditions.elementToBeClickable(By.xpath("//a[@previewendpoint='/markupFormatter/previewDescription']"))
@@ -134,14 +144,19 @@ public class ManageJenkinsPage3Test extends BaseTest {
                 ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='textarea-preview']"))
         );
 
+        getWait10().until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//div[@class='textarea-preview']//b")
+        ));
+
         WebElement boldText = previewArea.findElement(By.tagName("b"));
         Assert.assertEquals(boldText.getText(), "Bold");
         Assert.assertEquals(boldText.getCssValue("font-weight"), "700");
 
         WebElement italicText = previewArea.findElement(By.tagName("i"));
         Assert.assertEquals(italicText.getText(), "Italic");
+        Assert.assertEquals(italicText.getCssValue("font-style"), "italic");
 
-        String previewText = previewArea.getText();Assert.assertEquals(italicText.getCssValue("font-style"), "italic");
+        String previewText = previewArea.getText();
         Assert.assertEquals(previewText, "Bold Italic",
                 "Preview should display 'Bold Italic' without HTML tags");
     }
