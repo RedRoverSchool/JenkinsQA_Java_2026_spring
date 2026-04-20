acdfaae9a51c927727a551c2486cf75f4719b610
Natalia Karataeva <karataeva_natalia@mail.ru>
2026-04-20 23:07:54 +0300

    Add new test to FolderTest

--- src/test/java/school/redrover/Folder2Test.java
+++ src/test/java/school/redrover/Folder2Test.java
@@ -1,16 +1,34 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
 public class Folder2Test extends BaseTest {
 
+    public static final String FOLDER_NAME = "New Folder";
+
     @Test
     public void testNewItemPageOpen() {
         getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "New Item");
+    }
+
+    @Test (dependsOnMethods = "testNewItemPageOpen")
+    public void testNameEnterAndSelectFolderType() {
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+
+        WebElement enterItemNameField = getDriver().findElement(By.id("name"));
+        enterItemNameField.click();
+        enterItemNameField.sendKeys(FOLDER_NAME);
+
+        WebElement folderRadioButton = getDriver().findElement(By.xpath("//li[@class = 'com_cloudbees_hudson_plugins_folder_Folder']"));
+        folderRadioButton.click();
+
+        Assert.assertEquals(FOLDER_NAME, enterItemNameField.getAttribute("value"));
+        Assert.assertTrue(folderRadioButton.getAttribute("class").contains("active"));
     }
 }
