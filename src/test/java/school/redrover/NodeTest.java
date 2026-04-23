75f3dc4a9df82c0b9c41c49a8c6778536d2b313c
Pitstop-git <liudmilak@yandex.ru>
2026-04-23 20:54:08 +0300

    Delete a node

--- src/test/java/school/redrover/NodeTest.java
+++ src/test/java/school/redrover/NodeTest.java
@@ -71,7 +71,7 @@ public class NodeTest extends BaseTest {
 
         getWait5().until(ExpectedConditions.elementToBeClickable(
                 getDriver().findElement(By.xpath("//form [@action='markOffline']")))).click();
-        WebElement submitButton = getDriver().findElement(By.className("jenkins-submit-button"));
+        WebElement submitButton = getDriver().findElement(By.xpath("//button [@name = 'Submit']"));
         submitButton.click();
 
         Assert.assertEquals(getDriver().findElement(By.className("message")).getText(), "Disconnected by admin");
