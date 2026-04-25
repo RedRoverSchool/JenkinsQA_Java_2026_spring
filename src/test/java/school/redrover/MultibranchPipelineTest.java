5a036f7ecfce1044f885e9898dde0d3fd27e8a22
babadza <babadza@gmail.com>
2026-04-25 12:08:57 +0300

    Added testRename in MultibranchPipelineTest

--- src/test/java/school/redrover/MultibranchPipelineTest.java
+++ src/test/java/school/redrover/MultibranchPipelineTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
-import school.redrover.common.TestUtils;
+
 
 public class MultibranchPipelineTest extends BaseTest {
 
