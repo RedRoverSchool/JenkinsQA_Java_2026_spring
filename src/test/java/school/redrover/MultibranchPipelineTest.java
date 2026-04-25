df2f888105055dbe633ec4bc340290eebbd4fb63
babadza <babadza@gmail.com>
2026-04-25 12:08:26 +0300

    Added testRename in MultibranchPipelineTest

--- src/test/java/school/redrover/MultibranchPipelineTest.java
+++ src/test/java/school/redrover/MultibranchPipelineTest.java
@@ -7,10 +7,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+import school.redrover.common.TestUtils;
 
 public class MultibranchPipelineTest extends BaseTest {
 
 	private final static String PROJECT_NAME = "MultibranchPipelineProject";
+	private final static String PROJECT_NAME_1 = "MultibranchPipelineProject1";
 
 
 	@Test
@@ -32,4 +34,19 @@ public class MultibranchPipelineTest extends BaseTest {
 				ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'%s')]".formatted(PROJECT_NAME))));
 		Assert.assertEquals(actualProjectName.getText(), PROJECT_NAME);
 	}
+
+	@Test(dependsOnMethods ="testCreate" )
+	public void testRename(){
+		getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'%s')]".formatted(PROJECT_NAME)))).click();
+		getDriver().findElement(By.xpath("//a[contains(@href,'job') and ./span[text()='Rename']]")).click();
+		getDriver().findElement(By.xpath("//input[@name='newName']")).clear();
+		getDriver().findElement(By.xpath("//input[@name='newName']")).sendKeys(PROJECT_NAME_1);
+		getDriver().findElement(By.name("Submit")).click();
+		getWait5().until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath("//a[@class='app-jenkins-logo']"))).click();
+
+ 		Assert.assertEquals(getWait10().until(
+				ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(text(),'%s')]".formatted(PROJECT_NAME_1)))).getText(), PROJECT_NAME_1);
+	}
+
 }
