1283388705a04f1b7c2dbfb60d1d6685e04fbb64
Yerkezhan <m.erkosya@gmail.com>
2026-04-29 06:56:30 +0500

    updated by comments

--- src/test/java/school/redrover/SettingsOfViewTest.java
+++ src/test/java/school/redrover/SettingsOfViewTest.java
@@ -7,7 +7,7 @@ import school.redrover.common.BaseTest;
 
 public class SettingsOfViewTest extends BaseTest {
 
-    String textInput = "Test";
+    private static String textInput = "Test";
 
     @Test
     public void testCheckPlainTextInputFieldIsOpened(){
@@ -15,6 +15,16 @@ public class SettingsOfViewTest extends BaseTest {
         Assert.assertFalse(getDriver().findElements(By.name("description")).isEmpty());
         Assert.assertEquals(getDriver().findElement(By.className("textarea-preview-container")).getText(), "Plain text\n" +
                 "Preview");
+    }
+
+    @Test
+    public void testPreviewHideOptionIsAvailable() throws InterruptedException {
+        getDriver().findElement(By.id("description-link")).click();
+        getDriver().findElement(By.name("description")).sendKeys(textInput);
+        getDriver().findElement(By.className("textarea-show-preview")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.className("textarea-hide-preview")).isDisplayed());
+        Assert.assertEquals(getDriver().findElement(By.className("textarea-preview")).getText(), textInput);
     }
 
     @Test
