f570aa42768bdf3087d5825b4ccac5c78c6f4a68
yerkezhanm <m.erkosya@gmail.com>
2026-04-29 13:30:36 +0500

    Added new waits

--- src/test/java/school/redrover/SettingsOfViewTest.java
+++ src/test/java/school/redrover/SettingsOfViewTest.java
@@ -1,11 +1,14 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
 public class SettingsOfViewTest extends BaseTest {
+
+    private static String textInput = "Test";
 
     @Test
     public void testCheckPlainTextInputFieldIsOpened(){
@@ -13,5 +16,19 @@ public class SettingsOfViewTest extends BaseTest {
         Assert.assertFalse(getDriver().findElements(By.name("description")).isEmpty());
         Assert.assertEquals(getDriver().findElement(By.className("textarea-preview-container")).getText(), "Plain text\n" +
                 "Preview");
+    }
+
+    @Test
+    public void hidePreviewOptionIsAvailableTest() throws InterruptedException {
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.id("description-link"))).click();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.name("description")));
+
+        getDriver().findElement(By.name("description")).sendKeys(textInput);
+        getDriver().findElement(By.className("textarea-show-preview")).click();
+
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.className("textarea-hide-preview")));
+
+        Assert.assertTrue(getDriver().findElement(By.className("textarea-hide-preview")).isDisplayed());
+        Assert.assertEquals(getDriver().findElement(By.className("textarea-preview")).getText(), textInput);
     }
 }
