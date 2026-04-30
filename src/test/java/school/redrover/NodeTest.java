7ba5b4bf9bdcb0eef7ae84aebd705cb113f9cc6c
vida65979-arch <vida65979@gmai.com>
2026-04-30 16:38:02 +0600

    added BaseModel to page package and add new test

--- src/test/java/school/redrover/NodeTest.java
+++ src/test/java/school/redrover/NodeTest.java
@@ -88,6 +88,7 @@ public class NodeTest extends BaseTest {
                 ,"Mark this node temporarily offline");
     }
 
+    @Ignore
     @Test (dependsOnMethods = "testBringTheNodeBackOnline")
     public void testDeleteNode(){
 
