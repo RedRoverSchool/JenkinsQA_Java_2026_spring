884ce71a476834d63eefedd6419776c96f7439cc
AllaBird <allavorobieva@yahoo.com>
2026-05-03 14:07:56 -0400

    Refactor testRename to POM

---
+++ src/test/java/school/redrover/page/PipelineProjectRenamePage.java
@@ -0,0 +1,27 @@
+package school.redrover.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class PipelineProjectRenamePage extends BasePage {
+
+    public PipelineProjectRenamePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public PipelineProjectRenamePage updateProjectName(String name) {
+        WebElement inputField = getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='newName']")));
+        inputField.clear();
+        inputField.sendKeys(name);
+
+        return this;
+    }
+
+    public PipelineProjectPage clickRenameButton() {
+        getDriver().findElement(By.xpath("//button[@value='Rename']")).click();
+
+        return new PipelineProjectPage(getDriver());
+    }
+}
