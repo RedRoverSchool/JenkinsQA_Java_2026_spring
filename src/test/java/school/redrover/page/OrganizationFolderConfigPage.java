ae5950d7906eda83f1ba061ca50453869a73916f
NadiaSergeeva <nadechka.ten@gmail.com>
2026-05-04 13:54:37 +0200

    changed OrganizationFolderTest to POM

--- src/test/java/school/redrover/page/OrganizationFolderConfigPage.java
+++ src/test/java/school/redrover/page/OrganizationFolderConfigPage.java
@@ -1,9 +1,18 @@
 package school.redrover.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
 
 public class OrganizationFolderConfigPage extends BaseConfigPage{
     public OrganizationFolderConfigPage(WebDriver driver) {
         super(driver);
     }
+
+    public OrganizationFolderConfigPage enterDisplayName(String displayName) {
+        getDriver().findElement(By.xpath("//input[@name='_.displayNameOrNull']")).sendKeys(displayName);
+        return this;
+    }
+
+
 }
