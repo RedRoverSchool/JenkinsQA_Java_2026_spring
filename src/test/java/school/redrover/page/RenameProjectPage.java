c87e0fe2a33256bcce3d4d4601258f5fd1b0bdb0
babadza <babadza@gmail.com>
2026-05-07 09:39:46 +0300

    Added package import to the RenameProjectPage

--- src/test/java/school/redrover/page/RenameProjectPage.java
+++ src/test/java/school/redrover/page/RenameProjectPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import school.redrover.page.common.BasePage;
 
 public class RenameProjectPage extends BasePage {
 
