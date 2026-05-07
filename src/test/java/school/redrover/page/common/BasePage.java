bfd61bd416dd0f9587d80c077be0019534fceb11
Ilya_Alekseev <i.aliakseyeu@yandex.com>
2026-05-07 14:26:29 +0500

    clickManageButton-func-repair

--- src/test/java/school/redrover/page/common/BasePage.java
+++ src/test/java/school/redrover/page/common/BasePage.java
@@ -2,6 +2,7 @@ package school.redrover.page.common;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.page.HomePage;
 import school.redrover.page.ManagePage;
@@ -21,8 +22,9 @@ public class BasePage extends BaseModel {
     }
 
     public ManagePage clickManageButton() {
-        getDriver().findElement(By.id("root-action-ManageJenkinsAction")).click();
+        getWait10().until(ExpectedConditions.elementToBeClickable(By.id("root-action-ManageJenkinsAction"))).click();
         getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h1")));
+
         return new ManagePage(getDriver());
     }
 
