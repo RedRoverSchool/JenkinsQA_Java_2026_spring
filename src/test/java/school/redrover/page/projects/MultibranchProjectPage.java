c757f55ed55c9fdb724c90dff715760255743b16
Natalia Karataeva <karataeva_natalia@mail.ru>
2026-05-07 22:51:06 +0300

    Changing of renaming Multibranch Pipeline test according to pom

--- src/test/java/school/redrover/page/projects/MultibranchProjectPage.java
+++ src/test/java/school/redrover/page/projects/MultibranchProjectPage.java
@@ -2,6 +2,7 @@ package school.redrover.page.projects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.page.HomePage;
 import school.redrover.page.common.BaseProjectPage;
@@ -22,5 +23,27 @@ public class MultibranchProjectPage extends BaseProjectPage {
         getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@data-id='ok']"))).click();
 
         return new HomePage(getDriver());
+    }
+
+    public MultibranchProjectPage clickRename(String projectName) {
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("svg[tooltip='Multibranch Pipeline']")));
+        getDriver().findElement(By.xpath("//a[@href='/job/%s/confirm-rename']".formatted(projectName))).click();
+
+        return this;
+    }
+
+    public MultibranchProjectPage enterNewName(String newName) {
+        WebElement newNameEl = getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='newName']")));
+        newNameEl.clear();
+        newNameEl.sendKeys(newName);
+
+        return this;
+    }
+
+    public MultibranchProjectPage clickRenameButton() {
+        getDriver().findElement(By.xpath("//button[@value='Rename']")).click();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("svg[tooltip='Multibranch Pipeline']")));
+
+        return this;
     }
 }
