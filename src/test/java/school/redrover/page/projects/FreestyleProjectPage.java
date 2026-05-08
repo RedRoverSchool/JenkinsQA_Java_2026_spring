9d5d8e10ad212a3e89a90e6960908033b6b44bbd
babadza <babadza@gmail.com>
2026-05-08 12:57:24 +0300

    Refactoring FreestyleProjectTest class testAddBuildStepDropdownContainsAllOptions

--- src/test/java/school/redrover/page/projects/FreestyleProjectPage.java
+++ src/test/java/school/redrover/page/projects/FreestyleProjectPage.java
@@ -1,11 +1,19 @@
 package school.redrover.page.projects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.page.common.BaseProjectPage;
+import school.redrover.page.projectsConfig.FreestyleProjectConfigPage;
 
 public class FreestyleProjectPage extends BaseProjectPage {
 
     public FreestyleProjectPage(WebDriver driver) {
         super(driver);
+    }
+
+    public FreestyleProjectConfigPage clickConfigure(){
+        getDriver().findElement(By.xpath("//a[@href='/job/FreestyleProject/configure']")).click();
+        return new FreestyleProjectConfigPage(getDriver());
     }
 }
