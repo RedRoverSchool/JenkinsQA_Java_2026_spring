0b2921e876d8087e04beee18a0b76136bdcb9ea2
Nataliia M <plus380936644584@hotmail.com>
2026-05-08 18:01:03 +0300

    udp FindBy, new pages

--- src/test/java/school/redrover/page/CreateFolderViewPage.java
+++ src/test/java/school/redrover/page/CreateFolderViewPage.java
@@ -1,33 +1,54 @@
 package school.redrover.page;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import school.redrover.page.common.BasePage;
 import school.redrover.page.projects.FolderProjectPage;
 
 public class CreateFolderViewPage extends BasePage {
 
+    @FindBy(id = "name")
+    private WebElement elementName;
+
+    @FindBy(xpath = "//label[@for='hudson.model.ProxyView']")
+    private WebElement ratioGlobalView;
+
+    @FindBy(xpath = "//label[@for='hudson.model.ListView']")
+    private WebElement ratioListView;
+
+    @FindBy(xpath = "//label[@for='hudson.model.MyView']")
+    private WebElement ratioMyView;
+
+    @FindBy(id = "ok")
+    private WebElement buttonCreate;
+
     public CreateFolderViewPage(WebDriver driver) {
         super(driver);
     }
 
-    public CreateFolderViewPage setProjectName(String name) {
-        getDriver().findElement(By.id("name")).sendKeys(name);
+    public CreateFolderViewPage inputName(String name) {
+        elementName.sendKeys(name);
+        return this;
+    }
 
+    public CreateFolderViewPage chooseGlobalView() {
+        ratioGlobalView.click();
+        return this;
+    }
+
+    public CreateFolderViewPage chooseListView() {
+        ratioListView.click();
         return this;
     }
 
     public CreateFolderViewPage chooseMyView() {
-        getDriver().findElement(By.xpath("//label[@for='hudson.model.MyView']")).click();
-
+        ratioMyView.click();
         return this;
     }
 
     public FolderProjectPage clickCreateButton() {
-        getDriver().findElement(By.id("ok")).click();
-
+        buttonCreate.click();
         return new FolderProjectPage(getDriver());
     }
-
-
 }
