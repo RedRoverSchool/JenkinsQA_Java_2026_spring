326447676a0e958d210e253784f9a3bbc1057e4e
AllaBird <allavorobieva@yahoo.com>
2026-05-08 13:52:31 -0400

    Break test to check CI log

--- src/test/java/school/redrover/PipelineProjectTest.java
+++ src/test/java/school/redrover/PipelineProjectTest.java
@@ -64,7 +64,7 @@ public class PipelineProjectTest extends BaseTest {
                 .clickSaveButton()
                 .getDisabledWarningText();
 
-        Assert.assertTrue(warningText.contains("This11111111111111 project is currently disabled"));
+        Assert.assertTrue(warningText.contains("This project is currently disabled"));
     }
 
     @Test(dependsOnMethods = "testDisable")
@@ -118,7 +118,7 @@ public class PipelineProjectTest extends BaseTest {
                 .clickApply()
                 .getSaveText();
 
-        Assert.assertEquals(saveText, "Saved");
+        Assert.assertEquals(saveText, "11111111Saved");
     }
 
     @Test(dependsOnMethods = "testRename")
