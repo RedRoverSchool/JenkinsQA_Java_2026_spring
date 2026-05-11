af1743428f098a807953055517aa40ce2a2d22fa
Ilya_Alekseev <i.aliakseyeu@yandex.com>
2026-05-11 12:21:50 +0500

    testAddSecretTextCredentials

--- src/test/java/school/redrover/page/CredentialsPage.java
+++ src/test/java/school/redrover/page/CredentialsPage.java
@@ -3,11 +3,30 @@ package school.redrover.page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.page.common.BasePage;
 
 public class CredentialsPage extends BasePage {
-    public CredentialsPage(WebDriver driver) {super(driver);}
+
+    public CredentialsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//div[@class='jenkins-choice-list__item__label'][text()='Secret text']")
+    private WebElement secretTextButton;
+
+    @FindBy(xpath = "//button[text()='Next']")
+    private WebElement nextButton;
+
+    @FindBy(id = "cr-dialog-submit")
+    private WebElement createButton;
+
+    @FindBy(name = "_.secret")
+    private WebElement secretTextField;
+
+    @FindBy(name = "_.id")
+    private WebElement IDField;
 
     public CredentialsPage clickAddCredentialsButton() {
                 getWait5().until(ExpectedConditions.elementToBeClickable(
@@ -54,5 +73,36 @@ public class CredentialsPage extends BasePage {
 
     public boolean isCredentialDeleted(String id) {
         return  getWait10().until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//td[contains(text(), '" + id + "')]")));
+    }
+
+    public CredentialsPage clickSecretTextButton() {
+        secretTextButton.click();
+
+        return this;
+    }
+
+    public CredentialsPage clickNextButton() {
+        getWait10().until(ExpectedConditions.elementToBeClickable(nextButton)).click();
+        getWait10().until(ExpectedConditions.elementToBeClickable(createButton));
+
+        return this;
+    }
+
+    public CredentialsPage typeSecretText(String secret) {
+        secretTextField.sendKeys(secret);
+
+        return this;
+    }
+
+    public CredentialsPage typeID(String ID) {
+        IDField.sendKeys(ID);
+
+        return this;
+    }
+
+    public CredentialsPage clickCreateButton() {
+        createButton.click();
+
+        return this;
     }
 }
