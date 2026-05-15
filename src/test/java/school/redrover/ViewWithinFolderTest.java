0926a7d109537d6393ea1e5dfcf5e3b8a75594ed
Lixira <vrotangova@gmail.com>
2026-05-15 11:53:00 +0200

    test  testPreviewAddDescription to class ViewWithinFolderTest

--- src/test/java/school/redrover/ViewWithinFolderTest.java
+++ src/test/java/school/redrover/ViewWithinFolderTest.java
@@ -1,15 +1,20 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import school.redrover.common.TestUtils;
+import school.redrover.page.HomePage;
 import school.redrover.page.projects.FolderProjectPage;
 
 public class ViewWithinFolderTest extends BaseTest {
 
     private static final String FOLDER_NAME = "NewFolder";
     private static final String VIEW_NAME = "MyViewWithinFolder";
+    private static final String VIEW_DESCRIPTION = "Saved description";
 
     @Test
     public void testCreateMyView(){
@@ -21,5 +26,31 @@ public class ViewWithinFolderTest extends BaseTest {
                 .clickCreateButton()
                 .getCurrentViewName();
         Assert.assertEquals(nameView, VIEW_NAME);
+    }
+
+    @Test (dependsOnMethods = "testCreateMyView")
+    public void testPreviewAddDescription(){
+        String actualPreviewText = new HomePage(getDriver())
+                .clickOnProject(FOLDER_NAME, new FolderProjectPage(getDriver()))
+                .getViewByName(VIEW_NAME)
+                .addDescription(VIEW_DESCRIPTION)
+                .getTextPreview();
+
+        Assert.assertEquals(actualPreviewText, VIEW_DESCRIPTION);
+    }
+
+    @Test(dependsOnMethods = "testPreviewAddDescription")
+    public void testSaveViewDescription(){
+        String expectedDescription   = "Saved description";
+
+        new HomePage(getDriver()).clickOnProject(FOLDER_NAME, new FolderProjectPage(getDriver()));
+        getDriver().findElement(By.xpath("//a[normalize-space()='" + VIEW_NAME + "']")).click();
+        getDriver().findElement(By.id("description-link")).click();
+        getDriver().findElement(By.name("description")).sendKeys(expectedDescription);
+        getDriver().findElement(By.name("Submit")).click();
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.id("description-content")));
+        String actualDescription = getDriver().findElement(By.id("description-content")).getText();
+
+        Assert.assertEquals(actualDescription,expectedDescription);
     }
 }
