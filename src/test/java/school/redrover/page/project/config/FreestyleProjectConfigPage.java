35561c2725e41c666a7f6e8c7c468ca54de81db2
NadiaSergeeva <130238321+NadiaSergeeva@users.noreply.github.com>
2026-05-25 11:58:44 +0200

    restored not correctly removed code

--- src/test/java/school/redrover/page/project/config/FreestyleProjectConfigPage.java
+++ src/test/java/school/redrover/page/project/config/FreestyleProjectConfigPage.java
@@ -16,6 +16,11 @@ public class FreestyleProjectConfigPage extends BaseConfigPage<FreestyleProjectC
         super(driver);
     }
 
+    @Override
+    protected FreestyleProjectConfigPage self() {
+        return this;
+    }
+    
     public FreestyleProjectConfigPage fillDescription(String descriptiontext) {
         getWait10().until(ExpectedConditions.visibilityOfElementLocated(
                         By.name("description")))
