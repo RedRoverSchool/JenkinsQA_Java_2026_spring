cbedd64e16455922ca9ca3ef75ff8f6a346e787b
Nataliia.M <nataliia.mangasarian@icloud.com>
2026-05-27 00:37:45 +0300

    rm ConsoleTest to BuildHistoryTest

--- src/test/java/school/redrover/BuildHistoryTest.java
+++ src/test/java/school/redrover/BuildHistoryTest.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import school.redrover.page.HomePage;
-
 import java.util.List;
 
 public class BuildHistoryTest extends BaseTest {
@@ -36,10 +35,19 @@ public class BuildHistoryTest extends BaseTest {
 
         Assert.assertEquals(buildHistoryList.size(), 1);
         Assert.assertEquals(buildHistoryList.getFirst(), PROJECT_NAME);
-
     }
 
     @Test(dependsOnMethods = "testScheduledBuildAppearsInBuildHistory")
+    public void testSuccessMessageInConsole() {
+        String consoleText = new HomePage(getDriver())
+                .clickBuildHistory()
+                .clickConsole()
+                .getTextConsole();
+
+        Assert.assertTrue(consoleText.contains("Finished: SUCCESS"));
+    }
+
+    @Test(dependsOnMethods = "testSuccessMessageInConsole")
     public void testDeleteBuild() {
         List<String> buildHistoryList = new HomePage(getDriver())
                 .clickScheduleBuild(PROJECT_NAME)
