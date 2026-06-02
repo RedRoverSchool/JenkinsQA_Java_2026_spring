5109a8c818cafe7924cbcfc9dacf60c8cf7ac01b
NadiaSergeeva <nadechka.ten@gmail.com>
2026-06-02 09:41:39 +0200

    added new tests in OrganizationFolderTest

--- src/test/java/school/redrover/OrganizationFolderTest.java
+++ src/test/java/school/redrover/OrganizationFolderTest.java
@@ -20,6 +20,7 @@ public class OrganizationFolderTest extends BaseTest {
     public static final String ORG_FOLDER_DISPLAY_NAME = "OrgFolderDisplayName";
     public static final String FOLDER_NAME = "Folder new";
     public static final String DESCRIPTION_TEXT = "Description: New project";
+    public static final String FILE_NAME = "1.jar";
 
     @Test
     public void testCreate() {
@@ -36,6 +37,34 @@ public class OrganizationFolderTest extends BaseTest {
         Assert.assertEquals(joblist.getFirst(), ORG_FOLDER_NAME);
     }
 
+    @Test(dependsOnMethods = "testCreate")
+    public void testPipelineSyntax() {
+        boolean isTextContainsFileName = new HomePage(getDriver())
+                .clickOnProject(ORG_FOLDER_NAME, new OrganizationFolderPage(getDriver()))
+                .getSideMenu()
+                .clickPipelineSyntax()
+                .typeFilesToArchive(FILE_NAME)
+                .clickGenerateScript()
+                .isTextContainsFileName(FILE_NAME);
+
+        Assert.assertTrue(isTextContainsFileName);
+    }
+
+    @Test(dependsOnMethods = "testPipelineSyntax")
+    public void testCredentialsOpens() {
+        final List<String> expectedBreadcrumbs = List.of(ORG_FOLDER_NAME, "Credentials");
+
+        List<String> currentBreadcrumbs = new HomePage(getDriver())
+                .clickOnProject(ORG_FOLDER_NAME, new OrganizationFolderPage(getDriver()))
+                .getSideMenu()
+                .clickCredentials()
+                .getBreadcrumbs()
+                .getBreadcrumbItems();
+
+        Assert.assertEquals(currentBreadcrumbs.size(), 2);
+        Assert.assertTrue(currentBreadcrumbs.containsAll(expectedBreadcrumbs));
+    }
+
     @Ignore //bug in Jenkins (worked before)
     @Test(dependsOnMethods = "testCreate")
     public void testAddDescription() {
@@ -49,7 +78,7 @@ public class OrganizationFolderTest extends BaseTest {
         Assert.assertEquals(actualDescriptionText, DESCRIPTION_TEXT);
     }
 
-    @Test(dependsOnMethods = "testCreate")
+    @Test(dependsOnMethods = "testCredentialsOpens")
     public void testRename() {
         Boolean isUpdatedNameCorrect = new HomePage(getDriver())
                 .clickOnProject(ORG_FOLDER_NAME, new OrganizationFolderPage(getDriver()))
--- src/test/java/school/redrover/page/PipelineSyntaxPage.java
+++ src/test/java/school/redrover/page/PipelineSyntaxPage.java
@@ -1,11 +1,36 @@
 package school.redrover.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import school.redrover.page.common.BasePage;
+import school.redrover.page.project.OrganizationFolderPage;
 
 public class PipelineSyntaxPage extends BasePage {
 
+    @FindBy(xpath = "//input[@name='_.artifacts']")
+    private WebElement inputFilesToArchive;
+
+    @FindBy(id = "generatePipelineScript")
+    private WebElement generatePipelineScriptButton;
+
     public PipelineSyntaxPage(WebDriver driver) {
         super(driver);
+    }
+
+    public PipelineSyntaxPage typeFilesToArchive(String fileName) {
+        inputFilesToArchive.sendKeys(fileName);
+        return this;
+    }
+
+    public PipelineSyntaxPage clickGenerateScript() {
+        generatePipelineScriptButton.click();
+        return this;
+    }
+
+    public boolean isTextContainsFileName (String filename) {
+        return getDriver().findElement(By.xpath(" //textarea[@id='prototypeText']"))
+                .getAttribute("value").contains(filename);
     }
 }
