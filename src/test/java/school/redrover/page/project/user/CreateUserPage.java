c11e33bd99b86f767096152e1b3fdbd050247945
Yerkezhan <m.erkosya@gmail.com>
2026-06-12 07:31:54 +0500

    updated method clickCreateUserButton

--- src/test/java/school/redrover/page/project/user/CreateUserPage.java
+++ src/test/java/school/redrover/page/project/user/CreateUserPage.java
@@ -18,6 +18,7 @@ public class CreateUserPage extends BasePage {
     private final By emailField = By.name("email");
     private final By submitButton = By.name("Submit");
     private final By errorMessages = By.xpath("//div[@class = 'error jenkins-!-margin-bottom-2']");
+    private final By addUserButton = By.xpath("//div[@class='jenkins-app-bar__controls']");
 
     public CreateUserPage(WebDriver driver) {
         super(driver);
@@ -25,36 +26,44 @@ public class CreateUserPage extends BasePage {
 
     public CreateUserPage setUsername(String userName) {
         getDriver().findElement(usernameField).sendKeys(userName);
+
         return this;
     }
 
     public CreateUserPage setUserPassword(String userPassword) {
         getDriver().findElement(password1).sendKeys(userPassword);
+
         return this;
     }
 
     public CreateUserPage setConfirmUserPassword(String userPassword) {
         getDriver().findElement(password2).sendKeys(userPassword);
+
         return this;
     }
 
     public CreateUserPage setUserFullName(String userFullName) {
         getDriver().findElement(fullnameField).sendKeys(userFullName);
+
         return this;
     }
 
     public CreateUserPage setUserEmail(String userEmail) {
         getDriver().findElement(emailField).sendKeys(userEmail);
+
         return this;
     }
 
     public UserManagementPage clickCreateUserButton() {
-        getWait10().until(ExpectedConditions.elementToBeClickable(submitButton)).click();
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(submitButton)).click();
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(addUserButton));
+
         return new UserManagementPage(getDriver());
     }
 
     public CreateUserPage submitExpectingError() {
         getWait10().until(ExpectedConditions.elementToBeClickable(submitButton)).click();
+
         return this;
     }
 
