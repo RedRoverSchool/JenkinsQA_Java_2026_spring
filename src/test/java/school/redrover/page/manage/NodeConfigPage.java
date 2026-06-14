4577fc70a0909a6ddce117555d52fec4807a215d
Lixira <vrotangova@gmail.com>
2026-06-14 17:37:12 +0200

    Fix node usage dropdown click

--- src/test/java/school/redrover/page/manage/NodeConfigPage.java
+++ src/test/java/school/redrover/page/manage/NodeConfigPage.java
@@ -1,10 +1,15 @@
 package school.redrover.page.manage;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import school.redrover.page.common.BasePage;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,10 +35,21 @@ public class NodeConfigPage extends BasePage {
     }
 
     public NodeConfigPage changeUsage(String usage) {
-        getDriver().findElement(By.xpath("//select[@name='mode']")).click();
-        getDriver().findElement(By.xpath("//option[@value='%s']".formatted(usage))).click();
-        return this;
-    }
+
+            WebElement usageSelect = getDriver().findElement(By.xpath("//select[@name='mode']"));
+
+            ((JavascriptExecutor) getDriver()).executeScript(
+                    "arguments[0].scrollIntoView({block: 'center', inline: 'nearest'});",
+                    usageSelect
+            );
+
+            new WebDriverWait(getDriver(), Duration.ofSeconds(5))
+                    .until(ExpectedConditions.elementToBeClickable(usageSelect));
+
+            new Select(usageSelect).selectByValue(usage);
+
+            return this;
+        }
 
     public NodeConfigPage saveChanges() {
         getDriver().findElement(By.xpath("//button[@value='Save']")).click();
