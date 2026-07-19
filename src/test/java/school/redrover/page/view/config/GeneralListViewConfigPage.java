f126699c9a92bbcaf1f13187a4a74d5aed46eb78
Nataliia.M <nataliia.mangasarian@icloud.com>
2026-07-19 15:21:17 +0300

    upd saveButton

--- src/test/java/school/redrover/page/view/config/GeneralListViewConfigPage.java
+++ src/test/java/school/redrover/page/view/config/GeneralListViewConfigPage.java
@@ -1,22 +1,18 @@
 package school.redrover.page.view.config;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import school.redrover.page.view.GeneralViewPage;
 import school.redrover.page.view.base.BaseGeneralViewConfigPage;
 
 public class GeneralListViewConfigPage extends BaseGeneralViewConfigPage {
-
-    @FindBy(name = "Submit")
-    private WebElement buttonSave;
 
     public GeneralListViewConfigPage(WebDriver driver) {
         super(driver);
     }
 
     public GeneralViewPage clickSave() {
-        buttonSave.click();
+        getDriver().findElement(By.cssSelector("button[name='Submit']")).click();
         return new GeneralViewPage(getDriver());
     }
 }
