51c70516d348294c6a83a88890abdbb8579d7f57
Nataliia.M <nataliia.mangasarian@icloud.com>
2026-07-20 20:20:00 +0300

    upd

--- src/test/java/school/redrover/page/view/create/CreateGeneralViewPage.java
+++ src/test/java/school/redrover/page/view/create/CreateGeneralViewPage.java
@@ -2,6 +2,7 @@ package school.redrover.page.view.create;
 
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.page.view.GeneralViewPage;
 import school.redrover.page.view.config.GeneralListViewConfigPage;
 
@@ -14,14 +15,16 @@ public class CreateGeneralViewPage extends CreateViewBasePage<CreateGeneralViewP
     @Step("Select List View (General) and click Create")
     public GeneralListViewConfigPage selectListViewAndClickCreate() {
         ratioListView.click();
-        buttonCreate.click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(buttonCreate)).click();
+
         return new GeneralListViewConfigPage(getDriver());
     }
 
     @Step("Select My View (General) and click Create")
     public GeneralViewPage selectMyViewAndClickCreate() {
         ratioMyView.click();
-        buttonCreate.click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(buttonCreate)).click();
+
         return new GeneralViewPage(getDriver());
     }
 }
