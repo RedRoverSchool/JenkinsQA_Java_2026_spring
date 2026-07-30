e704541a0ad3c7254d0626792dbf15587dba5fad
edita.orlova <edita.orlova@solva.kz>
2026-07-31 00:48:00 +0200

    TestUpdateCred

--- src/test/java/school/redrover/page/manage/CredentialsPage.java
+++ src/test/java/school/redrover/page/manage/CredentialsPage.java
@@ -114,7 +114,7 @@ public class CredentialsPage extends BasePage {
     }
 
     public AddCredentialsPage clickUpdateCredentialButton(String credentialId) {
-        String updateCredentialLocator = "//div[@class='credentials-card__controls']/button[@data-type='credentials-update' and contains (@data-url, '/credential/%s/')]"
+        String updateCredentialLocator = "//div[@class='credentials-card__controls']/button[@data-type='credentials-update' and contains (@data-url, '/credential/%s')]"
                 .formatted(credentialId);
         getWait10().until(ExpectedConditions.elementToBeClickable(By.xpath(updateCredentialLocator))).click();
         return new AddCredentialsPage(getDriver());
